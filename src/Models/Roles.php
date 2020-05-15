d00bff030316ab8fca6ac467f1d82cd885bc2ec9
安康 <oncwnuKxUu1G1rRrYG3Ph2LVqV8c@git.weixin.qq.com>
2020-05-15 16:42:11 +0800

    权限管理

---
+++ src/Models/Roles.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/5/15
+ * Time: 16:37
+ */
+
+namespace wangcb\permission\Models;
+
+
+use think\Model;
+
+class Roles extends Model
+{
+
+}
